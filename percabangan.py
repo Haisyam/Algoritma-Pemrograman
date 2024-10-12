ee730336cddc9a854fbfa6d288195c4613a94104
Haisyam <bersholawat2019@gmail.com>
2024-10-12 11:40:38 +0700

    first commit

---
+++ percabangan.py
@@ -0,0 +1,10 @@
+# "Jika hari ini adalah hari libur, maka saya akan pergi ke pantai. Jika tidak, maka saya akan pergi ke kampus."
+
+# Program code percabangan menggunakan Python
+
+hari_ini = "libur"
+
+if hari_ini == "libur":
+    print("Saya akan pergi ke pantai.")
+else:
+    print("Saya akan pergi ke kampus.")
---
+++ perulangan.py
@@ -0,0 +1,11 @@
+# "Saya akan mengulangi proses mencuci pakaian sampai semua pakaian bersih."
+
+# Program code perulangan menggunakan Python
+
+pakaian_kotor = 5
+
+while pakaian_kotor > 0:
+    print("Mencuci pakaian...")
+    pakaian_kotor -= 1
+
+print("Semua pakaian sudah bersih!")
